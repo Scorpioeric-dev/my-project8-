c1aaa61286c45acaf91de8b80e789cef98030856
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-08-30 21:43:42 -0600

    post Fn

--- server/controller.js
+++ server/controller.js
@@ -1,27 +1,29 @@
-const data = require('./data');
-
-
-
+const data = require("./data");
+let id = data.length + 1;
 
 module.exports = {
-    getData(req,res){
-        // console.log('in the building')
-        res.status(200).send(data)
-    },
-    getDataById:(req,res) => {
-        const car = data.find(ele => ele.id === +(req.params.id))
-        if(!car){
-            return res.status(500).send('Car not in Database')
-        }
-        res.status(200).send(car)
+  getData(req, res) {
+    // console.log('in the building')
+    res.status(200).send(data);
+  },
+  getDataById: (req, res) => {
+    const car = data.find(ele => ele.id === +req.params.id);
+    if (!car) {
+      return res.status(500).send("Car not in Database");
     }
-}
-//const getProduct = (req,res) => {
-    // return specific item once found otherwise, undefined
-    //the const of item is finding the specific id of the product or otherwise returning null the parse int is converting the str into a number
-//     const item = products.find(val => val.id === +(req.params.id))
-//     if (!item){
-//         return res.status(500).send('Item not in list')
-//     }
-//     res.status(200).send(item)
-// }ntroller set up
+    res.status(200).send(car);
+  },
+  postData: (req, res) => {
+    let { model, type, year, img } = req.body;
+    data.push({ ...req.body, id, model, type, year, img });
+    id++;
+    res.status(200).send(data);
+  }
+};
+// addStyle(req, res) {
+//     // console.log(req.body)
+//     data.push({ ...req.body, id });
+//     id++;
+//     // console.log(data)
+//     res.status(200).send(data);
+//   }
--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 4444
 const ctrl = require('./controller')
 
+
+app.use(express.json())
 app.listen(port,()=> console.log(`Me and my ${port}`))
 
 app.get(`/api/cars`, ctrl.getData)
 app.get(`/api/car/:id`,ctrl.getDataById)
-
+app.post(`/api/cars`,ctrl.postData)
 
 
 
